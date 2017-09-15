28ead83239e5a092a2bedc4d7017f66cde76556c
John Apostolakis <john.apostolakis@cern.ch>
2017-09-15 15:39:39 +0200

    Migration from specific (Vc) backend to general 'VectorBackend' specified in the installation of VecGeom.
    Reintroduced vector/templated stepper classes.
    
    Added two types of Stepper, adapted from steppers created by Ananya
      - CashKarpRKF45: suppressing 'backend' & using Geant::Double_v instead
      - ClassicalRK4:  converted to 'backend-type', in place of 'backend'
    
    Test of generic CashKarp stepper in testCashKarpStepper
    exercising both scalar & vector.
    
    New templated test for Vector Cash Karp
    
    New tests:
        testVectorCashKarp.cxx for templated version of class.
        testScalarCashKarp.cxx
    
    -------------------------------------------------------------
    
    First working 'flexible' CashKarp
    
    CashKarp:   'flexible' version that can integrate 'scalar' or Double_v
       - First tests with scalar & Double_v work: results agree with
            original scalar, and between the two.
       - Ugly code (for now) to keep StepWithErrorEstimate method
            inside the class definition.
       - Extra flexible method: can inherit (as before) or not from
            GUVVectorIntegrationStepper
            Note: ugly #ifdef INHERITS  & other #defines (override, final)
    
         To-do:  tests with different charge in different lanes etc.
    
    test/testFlexibleCashKarp.cxx
       - Enabled test with scalar case.
    
    Renamed GUFieldTrack to ScalarFieldTrack
    Renamed GUIntegrationDriver to ScalarIntegrationDriver, and its test to testScalarIntegrationDriver
    
    -----------------------------------------
    
    New SimpleIntegrationDriver class
    - Added new methods InsertNewTrack (in dev) and StoreOuptut (to be revised.)
    - New test case with different termination, shorter hStep

---
+++ magneticfield/inc/FormattedReporter.h
@@ -0,0 +1,169 @@
+#include <fstream>
+#include <cstdlib>
+#include <iomanip>
+
+//  Auxiliary methods - should be encapsulated into a separate helper class
+
+namespace ReportValuesOfVectors
+{
+
+const int sDefaultNameLength= 14;
+const int sDefaultVarSize =   12;
+
+// using std::setw;
+// using std::cout;
+// using std::endl;
+
+template< typename Real_v >
+   void
+   ReportRowOfDoubles( std::string  varName,
+                       const Real_v varValue, 
+                       int          charName = -1,
+                       int          widthVal = -1 )
+{
+   using std::cout;
+   using std::endl;
+
+   // int argWV= widthVal;
+   if( charName <=  0 ) { charName  = sDefaultNameLength; }
+   if( widthVal <= 0 ) { widthVal = sDefaultVarSize; }
+
+   int prec = (widthVal - 7);
+   prec = std::min( 5, prec );
+   widthVal = prec + 7;
+   
+   std::cout << std::setw( charName ) << varName << " : ";
+   for (int i = 0; i < vecgeom::kVectorSize; ++i) {   
+      std::cout << " " << std::setw( widthVal )
+                << std::setprecision( prec )
+                << vecCore::Get( varValue, i ) << " | ";      
+   }
+   // Auxiliary information about width, precision
+   // cout << " withVal: arg= " << argWV << " used= " << widthVal << " ( prec= " << prec << " ) ";
+   // cout << " lenName = " << charName; 
+   cout << endl;
+}
+
+// ---------------------------------------------
+template< typename Real_v >
+   void
+   ReportRowOfSquareRoots( std::string  varName,
+                           const Real_v valueSq,      // Square of interesting value
+                           int          charName = -1,
+                           int          widthVal = -1 )
+{
+   if( vecCore::MaskEmpty( valueSq < 0.0)  ) {
+      Real_v value= vecCore::math::Sqrt( valueSq );
+      ReportRowOfDoubles( varName, value, charName, widthVal );
+   } else {
+      // There is an erroneous value !
+      std::string varNameAndWarning= "WARNING> some values are Negative> " + varName;
+      ReportRowOfDoubles( varNameAndWarning, valueSq, charName, widthVal );
+   }   
+}
+// ---------------------------------------------
+
+template< typename Real_v >
+   void
+   ReportManyRowsOfDoubles( std::string  varArrName,
+                            const Real_v varArr[],
+                            int          arrLen, 
+                            int          charName = -1,
+                            int          widthVal = -1 )
+{
+   for( int i=0; i< arrLen; i++ ) {
+      // std::ostringstream nameAndIndex;
+      // nameAndIndex << varArrName << "[" << i << "]/AF";
+      // ReportRowOfDoubles( nameAndIndex.str(), yValues[i] );
+      std::string nameAndIndex = varArrName + "[" + std::to_string(i) + "]"; // + "/AF";
+      ReportRowOfDoubles<Real_v>( nameAndIndex, varArr[i], charName, widthVal );
+   }
+   std::cout << "##-------------------------------------------------------------------------------" << std::endl;
+}
+
+// ---------------------------------------------
+
+
+template< typename Real_v >
+   void
+   ReportManyRowsOfPositionsMomenta( std::string  varName, 
+                                     const Real_v varPositionsMomenta[],
+                                     int          arrLen,
+                                     int          widthNm  = -1,
+                                     int          widthVal = -1 )
+{
+   using vecCore::math::Sqrt;
+
+   ReportManyRowsOfPositionsMomenta( varName,
+                                     varPositionsMomenta,
+                                     arrLen,
+                                     widthNm, 
+                                     widthVal );
+   
+   Real_v momEnd= Sqrt(yNext[3] * yNext[3] + yNext[4] * yNext[4] + yNext[5] * yNext[5]);
+   Real_v momStart= Sqrt( y[3] * y[3] + y[4] * y[4] + y[5] * y[5] );
+
+   Real_v diffMagP = momEnd - momStart;
+   ReportRowOfDoubles( "diff|p|", diffMagP );
+
+   double tinyVal = 1.0e-80;
+   Real_v relDiff = diffMagP / ( momStart + Real_v(tinyVal) ) ;
+   ReportRowOfDoubles( "d|p|/|p|",   relDiff  );
+
+   double thresholdRelativeDiff = 1.0e-5;   //  Later: 3 * epsilon ?? 
+   if( ! MaskEmpty( relDiff < 1.0e-5 * momStart ) )
+   {
+      int extraWidth= widthVal + 8;
+      ReportRowOfDoubles( "|momEnd|",   momEnd,   widthNm, extraWidth  );
+      ReportRowOfDoubles( "|momStart|", momStart, widthNm, extraWidth  );
+   }
+   std::cout << "##-------------------------------------------------------------------------------" << std::endl;
+}
+
+// ---------------------------------------------
+
+
+template< typename Real_v >
+   void
+   ReportRowOfBools( std::string  varName,
+                    const vecCore::Mask_v<Real_v> var,
+                    int          charName= -1,
+                    int          widthVal= -1)
+{
+   if( charName < 0 ) { charName  =  sDefaultNameLength; }
+   if( widthVal < 0 ) { widthVal = sDefaultVarSize; }
+   
+   std::cout << std::setw( charName ) << varName << " : ";
+   for (int i = 0; i < vecgeom::kVectorSize; ++i) {
+      std::cout << " " << std::setw( widthVal ) << vecCore::Get( var, i ) << " | ";
+   }
+   std::cout << std::endl;
+}
+
+// ---------------------------------------------
+
+
+// ===============  Selective Reporting / Printing ==================
+
+template< typename Real_v >
+void
+ReportRowOfDoublesIf( std::string    varName,
+                      const Real_v   var, 
+                      vecCore::Mask_v<Real_v> cond,
+                      int            charName = -1,
+                      int            widthVal = -1                 
+   )
+{
+   if( charName < 0 ) { charName  =  sDefaultNameLength; }
+   if( widthVal < 0 ) { widthVal = sDefaultVarSize; }
+   
+   std::cout << std::setw( charName ) << varName << " : ";
+   for (int i = 0; i < vecgeom::kVectorSize; ++i) {
+      if( Get( cond, i ) )
+         std::cout << " " << std::setw( widthVal ) << Get(var, i) << " | ";      
+   }
+   std::cout << std::endl;
+}
+
+
+};  // End of namespace
