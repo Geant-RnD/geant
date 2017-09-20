e1e04c207babcd1e90c2f35a293c34b0aa504f51
Marilena Bandieramonte <marilena.bandieramonte@cern.ch>
2017-09-20 19:37:38 +0200

    XSectionsVector class added to utility classes in realphysics

--- realphysics/physics/utils/inc/XSectionsVector.h
+++ realphysics/physics/utils/inc/XSectionsVector.h
@@ -4,39 +4,36 @@
 #include "EMModel.h"
 
 namespace geantphysics {
+    /**
+     * @brief   Class to store cross-sections table.
+     * @class   XSectionsVector
+     * @author  M Bandieramonte
+     * @date    September 2017
+     *
+     *
+     * \cite
+     */
 
-class Spline;
-/**
- * @brief   Class to handle tabulated cross-sections data -> one object per element for total cross-sections and one
- * object per subshell for partial cross-sections data.
- * @class   XSectionsVector
- * @author  M Bandieramonte
- * @date    September 2017
- *
- *
- * \cite
- */
+    class XSectionsVector{
+        
+    public:
+        XSectionsVector();
+        ~XSectionsVector();
+        
+        size_t FindCSBinLocation(double energy, size_t idx)const;
+        double GetValue(double energy, size_t& shellIdx);
+        double LinearInterpolation(double energy, size_t idx);
+        
+        
+        std::vector<double>   fBinVector;       //Cross sections bin vector (i.e. x coordinate)
+        std::vector<double>   fDataVector;      //Cross sections data vector (i.e. y coordinate)
+        
+        size_t numberOfNodes;                   // Number of elements
+        double edgeMin;                         // Energy of first point
+        double edgeMax;                         // Energy of last point
+        //Spline     *sp;                         // Spline interpolator
+        
+    };
+}
 
-class XSectionsVector {
-
-public:
-  XSectionsVector();
-  ~XSectionsVector();
-
-  size_t FindCSBinLocation(double energy, size_t idx) const;
-  double GetValueAt(double energy) const;
-  double GetValue(double energy, size_t &shellIdx) const;
-  inline double LinearInterpolation(double energy, size_t idx) const;
-
-  std::vector<double> fBinVector;  // Cross sections bin vector (i.e. x coordinate)
-  std::vector<double> fDataVector; // Cross sections data vector (i.e. y coordinate)
-
-  size_t fNumberOfNodes; // Number of elements
-  double fEdgeMin;       // Energy of first point
-  double fEdgeMax;       // Energy of last point
-  Spline *fSplineInt;    // Spline interpolator
-  int fCompID;           // id of the object
-};
-} // namespace geantphysics
-
-#endif // XSECTIONSVECTOR_H
+#endif //XSECTIONSVECTOR_H
--- realphysics/physics/utils/src/XSectionsVector.cc
+++ realphysics/physics/utils/src/XSectionsVector.cc
@@ -3,99 +3,63 @@
 using namespace std;
 namespace geantphysics {
 
-XSectionsVector::XSectionsVector()
-{
-  fBinVector  = std::vector<double>();
-  fDataVector = std::vector<double>();
-
-  fBinVector.clear();
-  fDataVector.clear();
-}
-
-XSectionsVector::~XSectionsVector() {}
-
-// Given and energy, retrieve the bin index corresponding to that energy
-size_t XSectionsVector::FindCSBinLocation(double energy, size_t idx) const
-{
-
-  size_t id = idx;
-  if (energy < fBinVector[1]) {
-    id = 0;
-    // return std::min(bin, numberOfNodes-2);
-  } else if (energy >= fBinVector[fNumberOfNodes - 2]) {
-    id = fNumberOfNodes - 2;
-  } // else
-  if (idx >= fNumberOfNodes || energy < fBinVector[idx] || energy > fBinVector[idx + 1]) {
-    // Bin location proposed by K.Genser (FNAL) from G4
-    id = std::lower_bound(fBinVector.begin(), fBinVector.end(), energy) - fBinVector.begin() - 1;
-    id = std::min(id, fNumberOfNodes - 2);
-  }
-  return id;
-}
-
-// Binary search algorithm to find the binIndex corresponding to 'energy'and retreiving the corresponding linear
-// interpolated value.
-double XSectionsVector::GetValueAt(double energy) const
-{
-
-  size_t idx;
-  int first, last, middle;
-  int upperm2 = fNumberOfNodes - 2;
-  // check if 'energy' is above/below the highes/lowest value
-  if (energy >= fBinVector[upperm2]) { // optimal
-    idx = upperm2;
-  } else if (energy <= fBinVector[1]) { // optimal
-    idx = 0;
-  } else {
-    // Perform a binary search to find the binIndex corresponding to 'energy'
-    first = 1;
-    last  = upperm2;
-    while (std::abs(last - first) > 1) {
-      middle = (first + last) / 2.;
-      if (energy < fBinVector[middle])
-        last = middle;
-      else
-        first = middle;
+    XSectionsVector::XSectionsVector()
+    {
+        fBinVector = std::vector<double>();
+        fDataVector = std::vector<double>();
+        
+        fBinVector.clear();
+        fDataVector.clear();
+        
     }
-    idx = last - 1;
-  }
-  // check if the 2 grid point is 0,0
-  if ((fDataVector[0] + idx) + (fDataVector[0] + idx + 1) == 0.0) {
-    return 0.0;
-  }
-  // std::cout<<"GetValueAt: "<<idx<<"\t"<<fBinVector[idx]<<"\t"<<fDataVector[idx]<<std::endl;
-  return fDataVector[idx] + (fDataVector[idx + 1] - fDataVector[idx]) * (energy - fBinVector[idx]) /
-                                (fBinVector[idx + 1] - fBinVector[idx]);
+    
+    XSectionsVector::~XSectionsVector(){}
+    
+    //Given and energy, retrieve the bin index corresponding to that energy
+    size_t XSectionsVector::FindCSBinLocation(double energy, size_t idx)const{
+        
+        size_t id = idx;
+        if(energy < fBinVector[1]) {
+            id=0;
+            //return std::min(bin, numberOfNodes-2);
+        } else if(energy >= fBinVector[numberOfNodes-2]) {
+            id = numberOfNodes - 2;
+        } //else
+        if(idx >= numberOfNodes || energy < fBinVector[idx]
+           || energy > fBinVector[idx+1])
+        {
+            // Bin location proposed by K.Genser (FNAL) from G4
+            id = std::lower_bound(fBinVector.begin(), fBinVector.end(), energy) - fBinVector.begin() - 1;
+            id = std::min(id, numberOfNodes-2);
+        }
+        return id;
+    }
+    
+    
+    
+    //_____________________________
+    //Given an energy, first retrieve the binIndex corresponding to that energy and then calculate the interpolated value (Linear Interpolation) corresponding to the data stored at that bin index
+    double XSectionsVector::GetValue(double energy, size_t& shellIdx){
+        
+        
+        if(energy <= edgeMin)
+        { shellIdx = 0; return fDataVector[0];}
+        if(energy >= edgeMax) {
+            shellIdx= numberOfNodes-1;
+            return fDataVector[shellIdx];
+        }
+        shellIdx=FindCSBinLocation(energy, shellIdx);
+        return LinearInterpolation(energy, shellIdx);
+
+    }
+    
+    
+    // Linear interpolation is used to get the interpolated value for lowEnergy cross sections (below K-shell binding energy).
+    //Before this method is called it is ensured that the energy is inside the bin
+    double XSectionsVector::LinearInterpolation(double energy, size_t idx)
+    {
+        return fDataVector[idx] +( fDataVector[idx + 1]-fDataVector[idx] ) * (energy - fBinVector[idx]) /( fBinVector[idx + 1]-fBinVector[idx] );
+    
+    }
+
 }
-
-//_____________________________
-// Given an energy, first retrieve the binIndex corresponding to that energy and then calculate the interpolated value
-// (Linear Interpolation) corresponding to the data stored at that bin index
-double XSectionsVector::GetValue(double energy, size_t &shellIdx) const
-{
-
-  if (energy <= fEdgeMin) {
-    shellIdx = 0;
-    // std::cout<<"GetValue:   "<<shellIdx<<"\t"<<fBinVector[shellIdx]<<"\t"<<fDataVector[shellIdx]<<std::endl;
-    return fDataVector[0];
-  }
-  if (energy >= fEdgeMax) {
-    shellIdx = fNumberOfNodes - 2;
-    // std::cout<<"GetValue:   "<<shellIdx<<"\t"<<fBinVector[shellIdx]<<"\t"<<fDataVector[shellIdx]<<std::endl;
-    return fDataVector[shellIdx];
-  }
-  shellIdx = FindCSBinLocation(energy, shellIdx);
-  // std::cout<<"GetValue:    "<<shellIdx<<"\t"<<fBinVector[shellIdx]<<"\t"<<fDataVector[shellIdx]<<std::endl;
-  return LinearInterpolation(energy, shellIdx);
-}
-
-// Linear interpolation is used to get the interpolated value for lowEnergy cross sections (below K-shell binding
-// energy).
-// Before this method is called it is ensured that the energy is inside the bin
-inline double XSectionsVector::LinearInterpolation(double energy, size_t idx) const
-{
-  return fDataVector[idx] + (fDataVector[idx + 1] - fDataVector[idx]) * (energy - fBinVector[idx]) /
-                                (fBinVector[idx + 1] - fBinVector[idx]);
-}
-
-} // end namespace geantphysics
