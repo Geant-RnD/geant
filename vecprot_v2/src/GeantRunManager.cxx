b7496560cb1438f5fecdcfa0b6833d274b9a86fb
Andrei Gheata <Andrei.Gheata@cern.ch>
2017-10-10 22:41:39 +0200

    Fixing releasing events.

--- vecprot_v2/src/GeantRunManager.cxx
+++ vecprot_v2/src/GeantRunManager.cxx
@@ -556,9 +556,9 @@ void GeantRunManager::RunSimulation() {
 Printf("=== Summary: %d propagators x %d threads: %ld primaries/%ld tracks,  total steps: %ld, snext calls: %ld, "
          "phys steps: %ld, mag. field steps: %ld, small steps: %ld, pushed: %ld, killed: %ld, bdr. crossings: %ld  RealTime=%gs CpuTime=%gs",
          fNpropagators, fNthreads, fEventServer->GetNprimaries(), ntransported, nsteps, nsnext, nphys, nmag, nsmall, npushed, nkilled, ncross, rtime, ctime);
-  LocalityManager *lmgr = LocalityManager::Instance();
-  Printf("NQUEUED = %d  NBLOCKS = %d NRELEASED = %d",
-         lmgr->GetNqueued(), lmgr->GetNallocated(), lmgr->GetNreleased());
+  //LocalityManager *lmgr = LocalityManager::Instance();
+  // Printf("NQUEUED = %d  NBLOCKS = %d NRELEASED = %d",
+  //       lmgr->GetNqueued(), lmgr->GetNallocated(), lmgr->GetNreleased());
 #ifdef USE_VECGEOM_NAVIGATOR
   Printf("=== Navigation done using VecGeom ====");
 #else
