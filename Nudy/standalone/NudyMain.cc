bdc832c166ea451b2d0f785690306785e27d8685
Abhijit Bhattacharyya <abhihere@gmail.com>
2018-01-16 19:00:37 +0100

    Updated standalone code...

--- Nudy/standalone/NudyMain.cc
+++ Nudy/standalone/NudyMain.cc
@@ -134,38 +134,5 @@ switch (cInput) {
   case 4: exit(0);
 }
 
-
-
-
-/*
-  double kinEnergy = 10.* geant::MeV;
-  double maxEnergy = 1000 * geant::GeV;
-  int particlePDG = -321;
-  //int particlePDG = 2212;
-  int Z = 82;
-  // number of nucleons
-  int N =207;
-  double txs, ixs, exs;
-
-  while (kinEnergy < maxEnergy)
-    {
-
-    txs = totxs.GetIsotopeCrossSection(geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetInternalCode(),
-			       kinEnergy, geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetPDGMass(), Z, N) / geant::millibarn;
-
-    ixs = inexs.GetIsotopeCrossSection(geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetInternalCode(),
-			       kinEnergy, geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetPDGMass(), Z, N) / geant::millibarn;
-
-    exs = elaxs.GetIsotopeCrossSection(geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetInternalCode(),
-			       kinEnergy, geantphysics::Particle::GetParticleByPDGCode(particlePDG)->GetPDGMass(), Z, N) / geant::millibarn;
-
-    std::cout <<"energyKin " << kinEnergy <<  " total " << txs << " elastic " << exs << " inelastic " << ixs <<  std::endl;
-
-    writef << kinEnergy/geant::MeV <<"  \t" << ixs << "  \t"
-     << exs << "  \t"<< 0 << "  \t"<< txs << std::endl;
-
-    kinEnergy *= 1.001;
-  }
-*/
   return 0;
 }
