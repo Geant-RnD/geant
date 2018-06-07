06c14fb78e28f066b546c4a35d8557b8a889ce3e
Vitalii Drohan <vitaliy.drogan@gmail.com>
2018-06-07 23:39:38 +0300

    Add Agner backend

--- core/base/inc/Geant/VectorTypes.h
+++ core/base/inc/Geant/VectorTypes.h
@@ -7,7 +7,7 @@
 
 namespace geant {
 
-using VectorBackend = vecgeom::VectorBackend;
+using VectorBackend = vecCore::backend::AngerAVX;
 typedef VectorBackend::Float_v Float_v;
 typedef VectorBackend::Double_v Double_v;
 typedef VectorBackend::Int_v Int_v;
